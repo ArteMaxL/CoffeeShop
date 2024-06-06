97fb39a814c5ae4adb09058f28202c3c96771f83
ArteMaxL <artemiolucero@gmail.com>
2024-06-05 23:58:03 -0300

    Modelo ShoppingCartItem

---
+++ CoffeeShop/Models/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShop.Models
+{
+    public class ShoppingCartItem
+    {
+        public Guid Id { get; set; }
+        public Product? Product { get; set; }
+        public int Quantity { get; set; }
+        public Guid ShoppingCartId { get; set; }
+    }
+}
